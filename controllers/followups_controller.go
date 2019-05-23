c5eafb54caef19c0ecbceaec621a519fb09a0ae6
masukomi <masukomi@masukomi.org>
2019-05-22 23:04:51 -0400

    added followups_controller

---
+++ controllers/followups_controller.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	// "log"
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"masukomi.org/junt/models"
+	"net/http"
+)
+
+type FollowupsController struct {
+	Db *gorm.DB
+}
+
+func (cc *FollowupsController) Create(w rest.ResponseWriter,
+	r *rest.Request) {
+
+	followup := models.Followup{}
+	if err := r.DecodeJsonPayload(&followup); err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := cc.Db.Save(&followup).Error; err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteJson(
+		map[string]interface{}{"status": "SUCCESS", "id": followup.Id})
+
+}
+
+func (cc *FollowupsController) FindById(w rest.ResponseWriter,
+	r *rest.Request) {
+
+	id := r.PathParam("id")
+	followup := models.Followup{}
+	if cc.Db.First(&followup, id).Error != nil {
+		rest.NotFound(w, r)
+		return
+	}
+	w.WriteJson(&followup)
+}
+
+func (cc *FollowupsController) ListAll(w rest.ResponseWriter,
+	r *rest.Request) {
+	followups := []models.Followup{}
+	cc.Db.Find(&followups)
+	w.WriteJson(&followups)
+}
