78d9724115f55ad2c3d88190bafef00efa9d6d04
masukomi <masukomi@masukomi.org>
2019-06-01 11:40:53 -0400

    added Followup#UpdateFromJson

--- models/followup.go
+++ models/followup.go
@@ -4,3 +4,8 @@ type Followup struct {
 	PeopleEvent
 	People []Person `gorm:"many2many:followups_people;"`
 }
+
+func (f *Followup) UpdateFromJson(data map[string]interface{}, db *gorm.DB) error {
+
+	return f.UpdatePeopleEventFromJson(data, db)
+}
