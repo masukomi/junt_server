042b0438f5098cd4494ea8358f21a0c19f008336
masukomi <masukomi@masukomi.org>
2019-06-01 16:55:16 -0400

    people now list company_ids

--- models/person.go
+++ models/person.go
@@ -34,7 +34,7 @@ type Person struct {
 	Email        string        `sql:"type:text;" json:"email"`
 	Phone        string        `sql:"type:text;" json:"phone"`
 	Note         string        `sql:"type:text;" json:"note"`           // markdown
-	CompanyId    int64         `json:"-"`                               // belongs_to Company
+	CompanyId    int64         `json:"company_id"`                      // belongs_to Company
 	CreatedAt    time.Time     `json:"created_at"`                      // generated if not supplied
 	UpdatedAt    time.Time     `json:"updated_at"`                      // generated if not supplied
 	Jobs         []Job         `json:"-" gorm:"many2many:jobs_people;"` // has and belongs to many jobs
