887f7fb5d82b0f82d1dcf326a6f00eb19f16072e
masukomi <masukomi@masukomi.org>
2019-06-11 11:59:36 -0400

    tweaked spec to run correctly again
    JuntTest is the new singleton class at the top of the file
    
    Refactored all the class variables out into properties of a singleton
    so as to kill all the warnings about using class variables.

--- spec/junt_server_spec.rb
+++ spec/junt_server_spec.rb
@@ -1,73 +1,99 @@
 require 'airborne'
 
-describe 'Junt' do
-  before(:all) do
-    ENV['JUNT_DB'] = "./junt_test.db"
+class JuntTest
+  attr_reader :server_pid, :base_url, :default_headers, :cached_data
+
+  def initialize()
     # if ENV['JUNT_DB'].nil? 
-    #   $stderr.puts("JUNT_DB env var must be specified.")
-    #   exit 1
+      ENV['JUNT_DB'] = "./junt_test.db"
     # end
-    # guarantee we're working with a fresh db
-    `rm #{ENV['JUNT_DB']}`
-    # start the junt server 
-    @@junt_server_pid = fork do
-      exec "./junt"
+    @base_url="http://localhost:8123/v1"
+    @default_headers = {}
+    @cached_data = {}
+  end
+  def start()
+    clean
+    boot
+  end
+  def clean
+    if File.exist? ENV['JUNT_DB']
+      `rm #{ENV['JUNT_DB']}`
     end
-    puts "about to sleep..."
-    # Process.detach(junt_server)
-    # give the server a couple seconds to create the db
-    # and start listening
-    sleep(2) 
+  end
+  def boot
+    @server_pid = fork do
+      exec "./junt_server"
+    end
+    puts "Server PID: #{@server_pid}"
+    sleep(2)
+  end
+  def shutdown
+    Process.kill("HUP", server_pid)
+  end
+  def self.begin()
+    @@jt = JuntTest.new() unless defined?(@@jt)
+    @@jt.start
+    @jt
+  end
+  def self.end()
+    @@jt.shutdown
+  end
+  def self.get()
+    if ! defined?(@@jt)
+      @@jt = JuntTest.new()
+    end
+    return @@jt
+  end
+end
 
+describe 'Junt' do
 
-    @@base_url = "http://localhost:8123/v1"
-    @@default_headers = {} # uses application/json content type by default
+  before(:all) do
+    JuntTest.begin()
   end
   after(:all) do
     # Kill the server
-    Process.kill("HUP", @@junt_server_pid)
-    # remove the test db
-    # `rm #{ENV['JUNT_DB']}`
+    JuntTest.end()
   end
   describe "initial lists" do
     it "should initially have no companies" do
-      get "#{@@base_url}/companies" # should return empty array
+      get "#{JuntTest.get.base_url}/companies" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no events" do
-      get "#{@@base_url}/events" # should return empty array
+      get "#{JuntTest.get.base_url}/events" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no followups" do
-      get "#{@@base_url}/followups" # should return empty array
+      get "#{JuntTest.get.base_url}/followups" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no homeworks" do
-      get "#{@@base_url}/homeworks" # should return empty array
+      get "#{JuntTest.get.base_url}/homeworks" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no interviews" do
-      get "#{@@base_url}/interviews" # should return empty array
+      get "#{JuntTest.get.base_url}/interviews" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no jobs" do
-      get "#{@@base_url}/jobs" # should return empty array
+      get "#{JuntTest.get.base_url}/jobs" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no offers" do
-      get "#{@@base_url}/offers" # should return empty array
+      get "#{JuntTest.get.base_url}/offers" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no people" do
-      get "#{@@base_url}/people" # should return empty array
+      get "#{JuntTest.get.base_url}/people" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no status_changes" do
-      get "#{@@base_url}/status_changes" # should return empty array
+      get "#{JuntTest.get.base_url}/status_changes" # should return empty array
       expect(json_body.size).to(eq(0))
     end
     it "should initially have no thanks_emails" do
-      get "#{@@base_url}/thanks_emails" # should return empty array
+      get "#{JuntTest.get.base_url}/thanks_emails" # should return empty array
       expect(json_body.size).to(eq(0))
     end
   end
@@ -79,15 +105,15 @@ describe 'Junt' do
         url: "https://example.com/test_co",
         note: "test co has 100% coverage"
       }
-      post "#{@@base_url}/companies", new_company_json, @@default_headers
+      post "#{JuntTest.get.base_url}/companies", new_company_json, JuntTest.get.default_headers
       expect_status(200)
       expect_json_types(status: :string, id: :int)
       expect(json_body[:status]).to(eq("SUCCESS"))
-      @@company_1_id = json_body[:id]
+      JuntTest.get.cached_data[:company_1_id] = json_body[:id]
     end
     it "should be able to create a job" do
       new_job_json = {
-        company_id: @@company_1_id,
+        company_id: JuntTest.get.cached_data[:company_1_id],
         job_title: "chief tester",
         posting_url: "https://example.com/test_co/chief_tester_job",
         source: "we work remotely",
@@ -97,109 +123,109 @@ describe 'Junt' do
         note: "a _markdown_ note",
         start_date: nil
       }
-      post "#{@@base_url}/jobs", new_job_json, @@default_headers
+      post "#{JuntTest.get.base_url}/jobs", new_job_json, JuntTest.get.default_headers
       expect_status(200)
       expect_json_types(status: :string, id: :int)
       expect(json_body[:status]).to(eq("SUCCESS"))
-      @@job_1_id = json_body[:id]
+      JuntTest.get.cached_data[:job_1_id] = json_body[:id]
     end
     it "should be able to create a new person" do
       new_person_json = {
         name: "mary smith",
-        email: 'msmith@@example.com',
+        email: 'msmith@example.com',
         phone: '+1 555-555-5555',
         note: 'another _markdown_ note',
       }
-      post "#{@@base_url}/people", new_person_json, @@default_headers
+      post "#{JuntTest.get.base_url}/people", new_person_json, JuntTest.get.default_headers
       expect_status(200)
       expect_json_types(status: :string, id: :int)
       expect(json_body[:status]).to(eq("SUCCESS"))
-      @@person_1_id = json_body[:id]
+      JuntTest.get.cached_data[:person_1_id] = json_body[:id]
     end
     describe "of events" do
       it "should be able to create a new followup" do
         new_followup_json = {
-          job_id: @@job_1_id,
-          person_ids: [@@person_1_id],
+          job_id: JuntTest.get.cached_data[:job_1_id],
+          person_ids: [JuntTest.get.cached_data[:person_1_id]],
           note: "a _markdown_ note"
         }
-        post "#{@@base_url}/followups", new_followup_json, @@default_headers
+        post "#{JuntTest.get.base_url}/followups", new_followup_json, JuntTest.get.default_headers
         expect_status(200)
         expect_json_types(status: :string, id: :int)
         expect(json_body[:status]).to(eq("SUCCESS"))
-        @@followup_1_id = json_body[:id]
+        JuntTest.get.cached_data[:followup_1_id] = json_body[:id]
       end
       it "should be able to create a new homework" do
         new_homework_json = {
-          job_id: @@job_1_id,
+          job_id: JuntTest.get.cached_data[:job_1_id],
           due_date: "2019-06-02T18:47:44+00:00",
           note: "a _markdown_ note"
         }
-        post "#{@@base_url}/homeworks", new_homework_json, @@default_headers
+        post "#{JuntTest.get.base_url}/homeworks", new_homework_json, JuntTest.get.default_headers
         expect_status(200)
         expect_json_types(status: :string, id: :int)
         expect(json_body[:status]).to(eq("SUCCESS"))
-        @@homework_1_id = json_body[:id]
+        JuntTest.get.cached_data[:homework_1_id] = json_body[:id]
       end
       it "should be able to create a new interview" do
         new_interview_json = {
-          job_id: @@job_1_id,
-          person_ids: [@@person_1_id],
+          job_id: JuntTest.get.cached_data[:job_1_id],
+          person_ids: [JuntTest.get.cached_data[:person_1_id]],
           note: "a _markdown_ note",
           scheduled_at: "2019-06-02T18:47:44+00:00",
           rating: "😃",
           type: "technical"
         }
-        post "#{@@base_url}/interviews", new_interview_json, @@default_headers
+        post "#{JuntTest.get.base_url}/interviews", new_interview_json, JuntTest.get.default_headers
         expect_status(200)
         expect_json_types(status: :string, id: :int)
         expect(json_body[:status]).to(eq("SUCCESS"))
-        @@interview_1_id = json_body[:id]
+        JuntTest.get.cached_data[:interview_1_id] = json_body[:id]
       end
       it "should be able to create a new offer" do
         new_offer_json = {
-          job_id: @@job_1_id,
+          job_id: JuntTest.get.cached_data[:job_1_id],
           status: "received",
           note: "a _markdown_ note"
         }
-        post "#{@@base_url}/offers", new_offer_json, @@default_headers
+        post "#{JuntTest.get.base_url}/offers", new_offer_json, JuntTest.get.default_headers
         expect_status(200)
         expect_json_types(status: :string, id: :int)
         expect(json_body[:status]).to(eq("SUCCESS"))
-        @@offer_1_id = json_body[:id]
+        JuntTest.get.cached_data[:offer_1_id] = json_body[:id]
       end
       it "should be able to create a new status_change" do
         new_status_change_json = {
-          job_id: @@job_1_id,
+          job_id: JuntTest.get.cached_data[:job_1_id],
           from: nil, 
           # initial status change for a job should be nil->something
           to: "applied",
           note: "a _markdown_ note"
         }
-        post "#{@@base_url}/status_changes", new_status_change_json, @@default_headers
+        post "#{JuntTest.get.base_url}/status_changes", new_status_change_json, JuntTest.get.default_headers
         expect_status(200)
         expect_json_types(status: :string, id: :int)
         expect(json_body[:status]).to(eq("SUCCESS"))
-        @@status_change_1_id = json_body[:id]
+        JuntTest.get.cached_data[:status_change_1_id] = json_body[:id]
       end
       it "should be able to create a new thanks_email" do
         new_thanks_email_json = {
-          job_id: @@job_1_id,
-          person_ids: [@@person_1_id],
+          job_id: JuntTest.get.cached_data[:job_1_id],
+          person_ids: [JuntTest.get.cached_data[:person_1_id]],
           note: "a _markdown_ note" 
         }
-        post "#{@@base_url}/thanks_emails", new_thanks_email_json, @@default_headers
+        post "#{JuntTest.get.base_url}/thanks_emails", new_thanks_email_json, JuntTest.get.default_headers
         expect_status(200)
         expect_json_types(status: :string, id: :int)
         expect(json_body[:status]).to(eq("SUCCESS"))
-        @@thanks_email_1_id = json_body[:id]
+        JuntTest.get.cached_data[:thanks_email_1_id] = json_body[:id]
       end
     end # end events creation
     
   end # end creation
   describe "showing objects" do
     it "should be able to show a company" do
-      get "#{@@base_url}/companies/#{@@company_1_id}"
+      get "#{JuntTest.get.base_url}/companies/#{JuntTest.get.cached_data[:company_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -214,7 +240,7 @@ describe 'Junt' do
       )
     end
     it "should be able to show a job" do
-      get "#{@@base_url}/jobs/#{@@job_1_id}"
+      get "#{JuntTest.get.base_url}/jobs/#{JuntTest.get.cached_data[:job_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -233,7 +259,7 @@ describe 'Junt' do
       )
     end
     it "should be able to show a person" do
-      get "#{@@base_url}/people/#{@@person_1_id}"
+      get "#{JuntTest.get.base_url}/people/#{JuntTest.get.cached_data[:person_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -248,7 +274,7 @@ describe 'Junt' do
     end
     # events....
     it "should be able to show a followup" do
-      get "#{@@base_url}/followups/#{@@followup_1_id}"
+      get "#{JuntTest.get.base_url}/followups/#{JuntTest.get.cached_data[:followup_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -260,7 +286,7 @@ describe 'Junt' do
       )
     end
     it "should be able to show a homework" do
-      get "#{@@base_url}/homeworks/#{@@homework_1_id}"
+      get "#{JuntTest.get.base_url}/homeworks/#{JuntTest.get.cached_data[:homework_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -272,7 +298,7 @@ describe 'Junt' do
       )
     end
     it "should be able to show a interview" do
-      get "#{@@base_url}/interviews/#{@@interview_1_id}"
+      get "#{JuntTest.get.base_url}/interviews/#{JuntTest.get.cached_data[:interview_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -288,7 +314,7 @@ describe 'Junt' do
       )
     end
     it "should be able to show an offer" do
-      get "#{@@base_url}/offers/#{@@offer_1_id}"
+      get "#{JuntTest.get.base_url}/offers/#{JuntTest.get.cached_data[:offer_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -300,7 +326,7 @@ describe 'Junt' do
       )
     end
     it "should be able to show an status_change" do
-      get "#{@@base_url}/status_changes/#{@@status_change_1_id}"
+      get "#{JuntTest.get.base_url}/status_changes/#{JuntTest.get.cached_data[:status_change_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
@@ -313,7 +339,7 @@ describe 'Junt' do
       )
     end
     it "should be able to show a thanks_email" do
-      get "#{@@base_url}/thanks_emails/#{@@thanks_email_1_id}"
+      get "#{JuntTest.get.base_url}/thanks_emails/#{JuntTest.get.cached_data[:thanks_email_1_id]}"
       expect_status(200)
       expect_json_types(
         id: :int,
